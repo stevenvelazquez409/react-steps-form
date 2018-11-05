00c70426dbd399d4645d7cff39780e892307e05c
Steven Velazquez <stevenvelazquez409@yahoo.com>
2018-11-05 16:04:38 -0500

    Initial commit

---
+++ src/components/Success.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+
+class Success extends Component {
+
+  //handles next step
+  continue = e => {
+    e.preventDefault();
+    //PROCESS FORM //
+    this.props.nextStep();
+  }
+
+  back = e => {
+      e.preventDefault();
+      this.props.prevStep();
+    }
+
+  render() {
+
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Success"/>
+          <h1>Thank you for your submission</h1>
+          <p>You will get an email for further instructions.</p>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+
+}
+
+
+export default Success;
